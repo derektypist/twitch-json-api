635202533458cef3b348f4ad0947de2c34489c9d
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-07-08 11:19:31 +0000

    Added makeURL function at script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Set Up Channels Array
+    let channels = ["ESL_SC2","OgamingSC2","cretetion",
+    "freecodecamp","storbeck","habathcx","RobotCaleb","noobs2ninjas"];
+
+    
+});
